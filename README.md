a8d741237f5617418835bebf5d2b5ed42c86ea0d
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-10 20:58:27 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# OffThePan
+
+A web app which recommends recipes based on the ingredients provided.
+
+Made using technologies such as Node.js, MongoDB, Express, ejs, TypeScript and JavaScript.
+
+## Features:
+
+- You can search for recipes directly by searching by the name of the dish, or by <b>the ingredients in the dish</b>.
+
+- You can specify the ingredients available with you and find yourself a recipe!
+
+- You can also search for recipes by the cuisine.
+
+- You can also filter out your intolerances in your results.
+
+- Different diets such as vegan, vegetarian, etc, too.
+
+- You can get wine recommendations to go with the food you choose. An amazon link will also be provided with the it so that you can buy if you don't have it.
+
+## To run:
+
+- Clone and cd into the repo.
+
+```
+git clone https://github.com/k4u5h4L/OffThePan.git && cd OffThePan
+```
+
+- Install the required dependencies
+
+```
+npm install
+```
+
+- Rename the `.env_sample` file to `.env` and fill in the needed API keys and data.
+
+- Start the development server by running
+
+```
+npm run dev
+```
+
+This should spin up the site on [localhost:3000](http://localhost:3000).
+
+## Note:
+
+- This project is made as a hobby and is <b>NOT</b> a commercial application.
+
+- Any contribution is welcome. You can fork and submit a pull request.
--- src/app.ts
+++ src/app.ts
@@ -25,21 +25,27 @@ app.set("view engine", "ejs");
 
 // console.log(quotes[0]);
 
-app.get("/", (req: Request, res: Response) => {
-    // console.log(data);
-    if (quotes.length == 0) {
-        const backup = {
-            text: "All you need is love. But a little chocolate now and then doesn't hurt.",
-            author: "Charles M. Schulz",
-        };
-        res.render("index", { data: backup });
-    } else {
-        const numberOfQuotes = quotes.length - 1;
-        const rand = Math.floor(Math.random() * numberOfQuotes); // generating a random number
-        const quoteData = quotes[rand];
+interface backupText {
+  text: string;
+  author: string;
+}
 
-        res.render("index", { data: quoteData });
-    }
+app.get("/", (req: Request, res: Response) => {
+  // console.log(data);
+  if (quotes.length == 0) {
+    const backup: backupText = {
+      text:
+        "All you need is love. But a little chocolate now and then doesn't hurt.",
+      author: "Charles M. Schulz",
+    };
+    res.render("index", { data: backup });
+  } else {
+    const numberOfQuotes: number = quotes.length - 1;
+    const rand: number = Math.floor(Math.random() * numberOfQuotes); // generating a random number
+    const quoteData = quotes[rand];
+
+    res.render("index", { data: quoteData });
+  }
 });
 
 // async function quoteSearch() {
@@ -52,166 +58,193 @@ app.get("/", (req: Request, res: Response) => {
 // }
 
 app.get("/about", (req: Request, res: Response) => {
-    res.render("about");
+  res.render("about");
 });
 
-app.route("/contact")
-    .get((req: Request, res: Response) => {
-        res.render("contact");
-    })
-    .post((req: Request, res: Response) => {
-        const contactInfo = {
-            name: req.body.name,
-            email: req.body.contactEmail,
-            subject: req.body.subject,
-            message: req.body.message,
-        };
+app
+  .route("/contact")
+  .get((req: Request, res: Response) => {
+    res.render("contact");
+  })
+  .post((req: Request, res: Response) => {
+    const contactInfo = {
+      name: req.body.name,
+      email: req.body.contactEmail,
+      subject: req.body.subject,
+      message: req.body.message,
+    };
 
-        let transport = nodemailer.createTransport({
-            host: "smtp.mailtrap.io",
-            port: 2525,
-            auth: {
-                user: process.env.EMAIL_ID,
-                pass: process.env.EMAIL_PASSWD,
-            },
-        });
-
-        const message = {
-            from: process.env.EMAIL_ID, // Sender address
-            to: "kaushal.bhat@yahoo.com", // List of recipients
-            subject: "Email from " + contactInfo.email + " about " + contactInfo.subject, // Subject line
-            text: contactInfo.message, // Plain text body
-        };
-        transport.sendMail(message, function (err, info) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(info);
-            }
-        });
-
-        res.render("thankyou", { data: contactInfo.name });
+    const transport = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASSWD,
+      },
     });
 
+    const message = {
+      from: process.env.EMAIL_ID, // Sender address
+      to: "kaushal.bhat@yahoo.com", // List of recipients
+      subject:
+        "Email from " + contactInfo.email + " about " + contactInfo.subject, // Subject line
+      text: contactInfo.message, // Plain text body
+    };
+    transport.sendMail(message, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    });
+
+    res.render("thankyou", { data: contactInfo.name });
+  });
+
 app.get("/test", (req: Request, res: Response) => {
-    res.render("reference");
+  res.render("reference");
 });
 
 app.post("/quickSearch", (req: Request, res: Response) => {
-    let search = req.body.searchItem.trim();
-    const initialUrl = "https://spoonacular.com/recipeImages/";
+  let search = req.body.searchItem.trim();
+  const initialUrl = "https://spoonacular.com/recipeImages/";
 
-    const tempVal = isValid(search);
+  const tempVal = isValid(search);
 
-    if (!tempVal) {
-        console.log("The input has more than a single word and/or contains symbols.");
-        res.render("failure", {
-            data: "Your input is not valid. Please try again withput symbols and special characters",
-        });
-        return false;
-    }
-
-    search = search.replace(/\s/g, "%20");
-    console.log(search);
-
-    // if you're using https module for api calls instead of fetch()
-    // https.get(apiUrl, (response) => {
-    //     console.log(response.statusMessage);
-
-    //     if (response.statusCode != 200) {
-    //         res.render("failure", { data: "We could not find anything what you searched for." });
-    //         return false;
-    //     }
-
-    //     response.on("data", (data) => {
-    //         try {
-    //             console.log(JSON.parse(data));
-    //         } catch (e) {
-    //             console.log("The file wasn't in a valid JSON format.");
-
-    //             res.render("failure", { data: "Sorry. Something went wrong on our end!" });
-    //             return false;
-    //         }
-
-    //         const searchData = JSON.parse(data);
-    //         const image = searchData.results[0].image;
-
-    //         // const imageUrl = initialUrl + searchData.results[0].id + "-636x393.jpg"; // use this if the other doesn't work
-    //         const imageUrl = initialUrl + image;
-
-    quickSearchData(search).then((data) => {
-        // console.log(data);
-        if (data.results.length == 0) {
-            res.render("failure", { data: "We could not find anything what you searched for." });
-        } else {
-            res.render("list", { searchData: data, query: search, imageUrl: initialUrl });
-        }
+  if (!tempVal) {
+    console.log(
+      "The input has more than a single word and/or contains symbols."
+    );
+    res.render("failure", {
+      data:
+        "Your input is not valid. Please try again withput symbols and special characters",
     });
+    return false;
+  }
 
-    //res.render("list", { data: searchData, query: search, imageLocation: imageUrl });
-    //     });
-    // });
+  search = search.replace(/\s/g, "%20");
+  console.log(search);
+
+  // if you're using https module for api calls instead of fetch()
+
+  // https.get(apiUrl, (response) => {
+  //     console.log(response.statusMessage);
+
+  //     if (response.statusCode != 200) {
+  //         res.render("failure", { data: "We could not find anything what you searched for." });
+  //         return false;
+  //     }
+
+  //     response.on("data", (data) => {
+  //         try {
+  //             console.log(JSON.parse(data));
+  //         } catch (e) {
+  //             console.log("The file wasn't in a valid JSON format.");
+
+  //             res.render("failure", { data: "Sorry. Something went wrong on our end!" });
+  //             return false;
+  //         }
+
+  //         const searchData = JSON.parse(data);
+  //         const image = searchData.results[0].image;
+
+  //         // const imageUrl = initialUrl + searchData.results[0].id + "-636x393.jpg"; // use this if the other doesn't work
+  //         const imageUrl = initialUrl + image;
+
+  quickSearchData(search).then((data) => {
+    // console.log(data);
+    if (data.results.length == 0) {
+      res.render("failure", {
+        data: "We could not find anything what you searched for.",
+      });
+    } else {
+      res.render("list", {
+        searchData: data,
+        query: search,
+        imageUrl: initialUrl,
+      });
+    }
+  });
+
+  //res.render("list", { data: searchData, query: search, imageLocation: imageUrl });
+  //     });
+  // });
 });
 
 async function quickSearchData(search: string) {
-    const apiUrl = `https://api.spoonacular.com/recipes/search?query=${search}&number=5&apiKey=${APIKey}`;
+  const apiUrl = `https://api.spoonacular.com/recipes/search?query=${search}&number=5&apiKey=${APIKey}`;
 
-    let response = await fetch(apiUrl);
-    let data = await response.json();
+  const response = await fetch(apiUrl);
+  const data = await response.json();
 
-    return data;
+  return data;
 }
 
 async function vegetarianSearch() {
-    const apiUrl = `https://api.spoonacular.com/recipes/search?number=5&diet=vegetarian&apiKey=${APIKey}`;
+  const apiUrl = `https://api.spoonacular.com/recipes/search?number=5&diet=vegetarian&apiKey=${APIKey}`;
 
-    let response = await fetch(apiUrl);
-    let data = await response.json();
+  const response = await fetch(apiUrl);
+  const data = await response.json();
 
-    return data;
+  return data;
 }
 
 app.get("/search", (req: Request, res: Response) => {
-    res.render("search");
+  res.render("search");
 });
 
 app.get("/vegetarian", (req: Request, res: Response) => {
-    vegetarianSearch().then((data) => {
-        // console.log(data);
-        const initialUrl = "https://spoonacular.com/recipeImages/";
-        if (data.results.length == 0) {
-            res.render("failure", { data: "We could not find anything what you searched for." });
-        } else {
-            res.render("list", { searchData: data, query: "Vegetarians", imageUrl: initialUrl });
-        }
-    });
+  vegetarianSearch().then((data) => {
+    // console.log(data);
+    const initialUrl = "https://spoonacular.com/recipeImages/";
+    if (data.results.length == 0) {
+      res.render("failure", {
+        data: "We could not find anything what you searched for.",
+      });
+    } else {
+      res.render("list", {
+        searchData: data,
+        query: "Vegetarians",
+        imageUrl: initialUrl,
+      });
+    }
+  });
 });
 
 app.post("/featured/:foodTitle", (req: Request, res: Response) => {
-    const searchTitle = req.params.foodTitle;
-    // if (searchTitle == "burger" || searchTitle == "salads" || searchTitle == "desserts") {
-    quickSearchData(searchTitle).then((data) => {
-        // console.log(data);
-        const initialUrl = "https://spoonacular.com/recipeImages/";
-        if (data.results.length == 0) {
-            res.render("failure", { data: "We could not find anything what you searched for." });
-        } else {
-            res.render("featuredList", { searchData: data, query: "burgers", imageUrl: initialUrl });
-        }
-    });
-    // } else {
-    //     res.render("failure", { data: "This page is not available!" });
-    // }
+  const searchTitle = req.params.foodTitle;
+  // if (searchTitle == "burger" || searchTitle == "salads" || searchTitle == "desserts") {
+  quickSearchData(searchTitle).then((data) => {
+    // console.log(data);
+    const initialUrl = "https://spoonacular.com/recipeImages/";
+    if (data.results.length == 0) {
+      res.render("failure", {
+        data: "We could not find anything what you searched for.",
+      });
+    } else {
+      res.render("featuredList", {
+        searchData: data,
+        query: searchTitle,
+        imageUrl: initialUrl,
+      });
+    }
+  });
+  // } else {
+  //     res.render("failure", { data: "This page is not available!" });
+  // }
 });
 
 app.post("/ingredientSearch", (req: Request, res: Response) => {
-    //let ingredients = req.body.ingredients;
-    let ingredients = APIfilter(req.body.ingredients);
+  //let ingredients = req.body.ingredients;
+  const ingredients = APIfilter(req.body.ingredients);
 
-    ingredientSearch(ingredients).then((data) => {
-        res.render("recipe", { searchData: data, searchQuery: req.body.ingredients });
-        // res.send(data);
+  ingredientSearch(ingredients).then((data) => {
+    res.render("recipe", {
+      searchData: data,
+      searchQuery: req.body.ingredients,
     });
+    // res.send(data);
+  });
 });
 
 // app.post("/nutrientSearch", (req: Request, res: Response) => {
@@ -224,82 +257,94 @@ app.post("/ingredientSearch", (req: Request, res: Response) => {
 // });
 
 app.post("/cuisineSearch", (req: Request, res: Response) => {
-    let cuisine = {
-        name: APIfilter(req.body.cuisine),
-        diet: req.body.diet,
-        intol: req.body.intol,
-    };
-    console.log(cuisine);
+  const cuisine = {
+    name: APIfilter(req.body.cuisine),
+    diet: req.body.diet,
+    intol: req.body.intol,
+  };
+  // console.log(cuisine);
 
-    cuisineSearch(cuisine).then((data) => {
-        res.render("recipe-cuisine", { searchData: data, searchQuery: req.body.cuisine });
-        // res.send(data);
+  cuisineSearch(cuisine).then((data) => {
+    res.render("recipe-cuisine", {
+      searchData: data,
+      searchQuery: req.body.cuisine,
     });
+    // res.send(data);
+  });
 });
 
 app.post("/wineSearch", (req: Request, res: Response) => {
-    //let ingredients = req.body.ingredients;
-    let ingredients = APIfilter(req.body.wineInput);
+  //let ingredients = req.body.ingredients;
+  const ingredients = APIfilter(req.body.wineInput);
 
-    wineSearch(ingredients).then((data) => {
-        // console.log(data);
+  wineSearch(ingredients).then((data) => {
+    // console.log(data);
 
-        res.render("wine", {
-            searchData: data,
-            searchQuery: "Wine with " + req.body.wineInput,
-        });
-
-        //res.send(data);
+    res.render("wine", {
+      searchData: data,
+      searchQuery: "Wine with " + req.body.wineInput,
     });
+
+    //res.send(data);
+  });
 });
 
 async function ingredientSearch(search: string) {
-    console.log(search);
-    // const apiUrl =
-    //     "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
-    //     search +
-    //     "number=5&apiKey=" +
-    //     APIKey;
+  console.log(search);
+  // const apiUrl =
+  //     "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
+  //     search +
+  //     "number=5&apiKey=" +
+  //     APIKey;
 
-    const apiUrl = `http://www.recipepuppy.com/api/?i=${search}`;
+  const apiUrl = `http://www.recipepuppy.com/api/?i=${search}`;
 
-    let response = await fetch(apiUrl);
-    let data = await response.json();
+  const response = await fetch(apiUrl);
+  const data = await response.json();
 
-    return data;
+  return data;
 }
 
 async function wineSearch(search: string) {
-    const apiUrl = `https://api.spoonacular.com/food/wine/pairing?food=${search}&apiKey=${APIKey}`;
+  const apiUrl = `https://api.spoonacular.com/food/wine/pairing?food=${search}&apiKey=${APIKey}`;
 
-    let response = await fetch(apiUrl);
-    let data = await response.json();
+  const response = await fetch(apiUrl);
+  const data = await response.json();
 
-    return data;
+  return data;
 }
 
-async function cuisineSearch(search: any) {
-    let intolerance = "";
-    let apiUrl = "";
+interface cuisineData {
+  name: string;
+  diet: string;
+  intol: string;
+}
 
-    if (search.intol == "none") {
-        intolerance = "#";
-    }
+async function cuisineSearch(search: cuisineData) {
+  console.log(search);
+  let intolerance = "";
+  let apiUrl = "";
 
-    if (search.diet == "whole30") {
-        apiUrl = `https://api.spoonacular.com/recipes/search?cuisine=${search.name}&number=10&apiKey=${APIKey}&intolerances=${intolerance}`;
-    } else {
-        apiUrl = `https://api.spoonacular.com/recipes/search?cuisine=${search.name}&number=10&apiKey=${APIKey}&diet=${search.diet}&intolerances=${intolerance}`;
-    }
+  if (search.intol == "none") {
+    intolerance = "#";
+  }
 
-    let response = await fetch(apiUrl);
-    let data = await response.json();
+  if (search.diet == "whole30") {
+    apiUrl = `https://api.spoonacular.com/recipes/search?cuisine=${search.name}&number=10&apiKey=${APIKey}&intolerances=${intolerance}`;
+  } else {
+    apiUrl = `https://api.spoonacular.com/recipes/search?cuisine=${search.name}&number=10&apiKey=${APIKey}&diet=${search.diet}&intolerances=${intolerance}`;
+  }
 
-    return data;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  return data;
 }
 
 app.get("*", (req: Request, res: Response) => {
-    res.render("failure", { data: "This page is not available!" });
+  res.render("failure", { data: "This page is not available!" });
 });
 
-app.listen(process.env.PORT || PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+);
